3b858971e2e69f41d8610beb3849cc0e791824b1
tomi <bustosth@gmail.com>
2024-05-21 09:25:05 -0300

    dias

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd, datetime as dt
 
 df = pd.read_excel('grilla.xlsx')
 
@@ -10,10 +10,14 @@ st.title("Grilla")
 st.sidebar.subheader("Filtros")
 
 # Filtro por Nombre
-carrera = st.sidebar.multiselect("Carrera", options=df['Carrera'].unique(), default=df['Carrera'].unique())
+carrera = st.sidebar.multiselect("Carrera", options=df['Carrera'].unique(), default=['SOCIO','COMU']) # default = df['Carrera'].unique()
 
 # Filtro por Dia
-dia = st.sidebar.multiselect("Dia", options=df['Dia'].unique(), default=df['Dia'].unique())
+dic = {'0':'lunes','1':'martes','2':'miercoles','3':'jueves','4':'viernes'}
+hoy = dt.datetime.now().weekday()
+for k in dic.keys():
+    hoy = str(hoy).replace(k, dic[k])
+dia = st.sidebar.multiselect("Dia", options=df['Dia'].unique(), default=[hoy]) # df['Dia'].unique()
 
 # Filtro por horario
 hs_min, hs_max = st.sidebar.slider("Horario", min_value=int(df['Horario'].min()), max_value=int(df['Horario'].max()),value=(int(df['Horario'].min()), int(df['Horario'].max())))
