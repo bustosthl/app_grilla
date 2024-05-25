1cd04cd2d08ef91758b520f631a90c0a3d4e8ded
tomi <bustosth@gmail.com>
2024-05-25 16:25:53 -0300

    logo

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import pandas as pd, datetime as dt
+from streamlit_javascript import st_javascript
 
 df = pd.read_excel('grilla.xlsx')
 
+st_theme = st_javascript("""window.getComputedStyle(window.parent.document.getElementsByClassName("stApp")[0]).getPropertyValue("color-scheme")""")
+if st_theme == "dark":
+    ruta_imagen = 'images/emergente pp logo blanco.png'
+else:
+    ruta_imagen = 'images/emergente pp logo negro.png'
+
 # Título de la aplicación
-st.title("Grilla")
+st.title("Grilla Sociales-UBA")
 
 # Sección de filtros
 st.sidebar.subheader("Filtros")
@@ -13,13 +20,16 @@ st.sidebar.subheader("Filtros")
 carrera = st.sidebar.multiselect("Carrera", options=df['Carrera'].unique(), default=['SOCIO','COMU']) # default = df['Carrera'].unique()
 
 # Filtro por clave
-clave = st.sidebar.multiselect("Clave", options=df['Materia clave'].unique(), default=['si']) 
+clave = st.sidebar.checkbox('Materias clave', True)
+if clave:
+    filtro_clave = df['Materia clave'].isin(['si'])
+else:
+    filtro_clave = df['Materia clave'].isin(['si','no'])
 
 # Filtro por Dia
 dic = {'0':'lunes','1':'martes','2':'miercoles','3':'jueves','4':'viernes'}
 hoy = '0'
 hoy = dt.datetime.now().weekday()
-print(hoy, type(hoy))
 if hoy >= 5: 
     hoy = '0'
     for k in dic.keys():
@@ -32,7 +42,8 @@ hs_min, hs_max = st.sidebar.slider("Horario", min_value=int(df['Horario'].min())
 # Aplicar filtros
 df_filtrado = df[
     (df['Carrera'].isin(carrera)) &
-    (df['Materia clave'].isin(clave)) &
+    #(df['Materia clave'].isin(clave)) &
+    (filtro_clave) &
     (df['Dia'].isin(dia)) &
     (df['Horario'] >= hs_min) &
     (df['Horario'] <= hs_max)
@@ -52,9 +63,10 @@ def color(value):
         return
     return f'background-color: {color}'
 
-df_style = df_filtrado.style.applymap(color, subset=['Carrera']).background_gradient(subset=['Horario'], cmap='Greys')
+df_style = df_filtrado.style.map(color, subset=['Carrera']).background_gradient(subset=['Horario'], cmap='Greys')
 
 st.dataframe(df_filtrado, hide_index=True, use_container_width=True, )
 
 # Mostrar el número de resultados
 st.write(f"Total de resultados: {df_filtrado.shape[0]}")
+st.image(ruta_imagen)
