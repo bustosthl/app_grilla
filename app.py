0c3c9ed0f738a07c73060a8aec4e6e791b9de27b
tomi <bustosth@gmail.com>
2024-05-25 15:02:05 -0300

    materias clave

--- app.py
+++ app.py
@@ -12,10 +12,15 @@ st.sidebar.subheader("Filtros")
 # Filtro por Nombre
 carrera = st.sidebar.multiselect("Carrera", options=df['Carrera'].unique(), default=['SOCIO','COMU']) # default = df['Carrera'].unique()
 
+# Filtro por clave
+clave = st.sidebar.multiselect("Clave", options=df['Materia clave'].unique(), default=['si']) 
+
 # Filtro por Dia
 dic = {'0':'lunes','1':'martes','2':'miercoles','3':'jueves','4':'viernes'}
 hoy = dt.datetime.now().weekday()
 for k in dic.keys():
+    if int(hoy) >= 5: 
+        hoy = '4'
     hoy = str(hoy).replace(k, dic[k])
 dia = st.sidebar.multiselect("Dia", options=df['Dia'].unique(), default=[hoy]) # df['Dia'].unique()
 
@@ -25,6 +30,7 @@ hs_min, hs_max = st.sidebar.slider("Horario", min_value=int(df['Horario'].min())
 # Aplicar filtros
 df_filtrado = df[
     (df['Carrera'].isin(carrera)) &
+    (df['Materia clave'].isin(clave)) &
     (df['Dia'].isin(dia)) &
     (df['Horario'] >= hs_min) &
     (df['Horario'] <= hs_max)
@@ -45,7 +51,8 @@ def color(value):
     return f'background-color: {color}'
 
 df_style = df_filtrado.style.applymap(color, subset=['Carrera']).background_gradient(subset=['Horario'], cmap='Greys')
-st.dataframe(df_filtrado, hide_index=True, use_container_width=True)
+
+st.dataframe(df_filtrado, hide_index=True, use_container_width=True, )
 
 # Mostrar el número de resultados
 st.write(f"Total de resultados: {df_filtrado.shape[0]}")
